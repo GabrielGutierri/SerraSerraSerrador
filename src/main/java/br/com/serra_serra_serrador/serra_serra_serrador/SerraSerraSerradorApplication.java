d316fe472b63cba7c6e900dedfc64b4b5c2ef535
Gabriel Gutierri <98917672+GabrielGutierri@users.noreply.github.com>
2023-10-07 17:34:31 -0300

    Serra o papo do vovô

---
+++ src/main/java/br/com/serra_serra_serrador/serra_serra_serrador/SerraSerraSerradorApplication.java
@@ -0,0 +1,13 @@
+package br.com.serra_serra_serrador.serra_serra_serrador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SerraSerraSerradorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SerraSerraSerradorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/serra_serra_serrador/serra_serra_serrador/SerraSerraSerradorApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.serra_serra_serrador.serra_serra_serrador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SerraSerraSerradorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
