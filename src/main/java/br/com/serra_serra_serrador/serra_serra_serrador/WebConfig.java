68bfd2bfef611875108805c9da12fb2e4b28682b
Gabriel Gutierri <98917672+GabrielGutierri@users.noreply.github.com>
2023-10-08 18:44:18 -0300

    Arrumando layout da página principal

---
+++ src/main/java/br/com/serra_serra_serrador/serra_serra_serrador/WebConfig.java
@@ -0,0 +1,14 @@
+package br.com.serra_serra_serrador.serra_serra_serrador;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**")
+                .addResourceLocations("classpath:/static/");
+    }
+}
