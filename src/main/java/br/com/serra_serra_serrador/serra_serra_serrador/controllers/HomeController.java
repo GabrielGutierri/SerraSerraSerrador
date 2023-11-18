66becc110d6c0cd793a3dbc57c0106a53f553a70
Vinicius Afonso <103777222+ViniciusAfonsoDosSantos@users.noreply.github.com>
2023-11-18 15:54:42 -0300

    Realizado pequenas alterações

--- src/main/java/br/com/serra_serra_serrador/serra_serra_serrador/controllers/HomeController.java
+++ src/main/java/br/com/serra_serra_serrador/serra_serra_serrador/controllers/HomeController.java
@@ -1,23 +1,12 @@
 package br.com.serra_serra_serrador.serra_serra_serrador.controllers;
 
-import br.com.serra_serra_serrador.serra_serra_serrador.models.GraphRequestModel;
-import br.com.serra_serra_serrador.serra_serra_serrador.models.GraphResponseModel;
-import br.com.serra_serra_serrador.serra_serra_serrador.services.GraphService;
 import br.com.serra_serra_serrador.serra_serra_serrador.services.GraphServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.view.RedirectView;
 
 import java.util.*;
 
-//@RestController
-//@RequestMapping("/api")
 @Controller
 public class HomeController {
     //criar serviços para cada tipo de gráfico
